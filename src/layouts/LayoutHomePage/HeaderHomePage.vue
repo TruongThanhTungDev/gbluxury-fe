bb3f7bed13315c596647aa6afee2823ae5aa40a7
Trương Thanh Tùng <88636809+TruongThanhTungDev@users.noreply.github.com>
2025-02-23 17:24:56 +0700

    update responsive

--- src/layouts/LayoutHomePage/HeaderHomePage.vue
+++ src/layouts/LayoutHomePage/HeaderHomePage.vue
@@ -14,7 +14,7 @@
     <div class="w-full img-background-container relative">
       <div class="background-header z-[1]"></div>
       <!-- <img src="../../assets/images/Phong-lam-viec-banner.jpg.webp" class="h-full w-full object-cover" alt=""> -->
-      <a-carousel class="w-full">
+      <a-carousel class="w-full" autoplay>
         <img v-for="item of listCarousel" :key="item" :src="require(`../../assets/images/${item}`)"  alt="back" class="img-background-container w-full object-cover">
       </a-carousel>
       <img src="../../assets/logo.png" alt="" id="logo-banner" class="z-[2]" :class="isShowLogo ? 'logo-header-banner-fixed' : 'logo-header-banner'" @click="toRouter">
@@ -89,14 +89,15 @@ export default {
       isShowMenu: false,
       isScroll: false,
       listCarousel: [
-        'crs0.png',
-        'crs5.png',
-        'crs4.png',
-        'crs2.png',
-        'crs1.png',
-        'crs3.png',
-        'crs6.png',
-        'crs7.png'
+        '1.png',
+        '2.png',
+        '3.png',
+        '4.png',
+        '5.png',
+        '6.png',
+        '7.png',
+        '8.png',
+        '10.png'
       ]
     };
   },
